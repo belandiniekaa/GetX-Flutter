9c59cd4557f720ea7e0b67918b95987487296aeb
Eka Belandini <eka.belandini@mhs.unsoed.ac.id>
2024-11-06 21:39:03 +0700

    getx

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  var isLogged = false.obs;
+  void login(String username, String password) {
+    if (username == 'H1D022002' && password == '123') {
+      isLogged.value = true;
+      Get.offAllNamed('/home');
+    } else {
+      Get.snackbar('Error', 'Username atau password salah');
+    }
+  }
+}
